63aec2e90978dd137adbbe1d6c27c83d6d10eb1f
cono200 <tatattian04@gmail.com>
2023-09-24 19:24:11 -0700

    Agregar archivos de proyecto.

---
+++ EstructuraControl.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosEric20Estructura
+{
+    internal class EstructuraControl
+    {
+        public string DiaSemana(int dia)
+        {
+            string semana;
+
+            switch (dia)
+            {
+                case 1:
+                    dia = 1;
+                    semana = "Domingo";
+                    break;
+                case 2:
+                    dia = 2;
+                    semana = "Lunes";
+                    break;
+                case 3:
+                    dia = 4;
+                    semana = "Martes";
+                    break;
+                case 4:
+                    dia = 5;
+                    semana = "Miercoles";
+                    break;
+                case 5:
+                    dia = 6;
+                    semana = "Jueves";
+                    break;
+                case 6:
+                    dia = 7;
+                    semana = "Viernes";
+                    break;
+                case 7:
+                    dia = 8;
+                    semana = "Sabado";
+                    break;
+                default:
+                    semana = "No existe ese dia karnal, no inventes cosas";
+                    break;
+            }
+            return semana;
+
+
+        }
+
+        public double PromedioNumeros(double numero1, double numero2, double numero3)
+        {
+            double promedioNumero = 0;
+            if (numero1 < 0 && numero2 <0 && numero3<0 )
+            {
+                return promedioNumero;
+            }
+            else if (numero1 < 0 && numero2<0 && numero3>0 )
+            {
+                promedioNumero = numero3;
+            }
+            else if (numero1 <0 && numero2>0 && numero3>0)
+            {
+                promedioNumero = (numero2 + numero3) / 2;
+            }
+            else if(numero1>0 && numero2>0 && numero3 < 0)
+            {
+                promedioNumero = (numero2 + numero1) / 2;
+            }
+            else
+            {
+                promedioNumero=(numero2 + numero3 + numero1) / 3;
+            }
+            return promedioNumero;
+            
+
+        }
+
+
+        public void Divisirve(double numero)
+        {
+
+            if (numero % 2 !=0 && numero % 3 !=0)
+            {
+                Console.WriteLine("No es karnal");
+            }
+
+            else if (numero %2 == 0 && numero %3 ==0)
+            {
+                Console.WriteLine("Se puede dividir tanto con 2 como por 3");
+            }
+            else if (numero % 2 == 0)
+            {
+                Console.WriteLine("Se puede dividir por 2");
+            }
+
+            else
+            {
+                Console.WriteLine("Se puede dividir entre 3");
+            }
+            /*
+             if (numero % 2 == 0 && numero % 3 == 0)
+            {
+                Console.WriteLine("Se puede dividir tanto con 2 y con 3");
+            }
+            else  if (numero % 2 == 0)
+            {
+                Console.WriteLine("Se puede dividir  por 2");
+            }
+            else if (numero % 3 == 0)
+            {
+                Console.WriteLine("Se puede dividir por 3");
+            }
+            else
+            {
+                Console.WriteLine("No ai");
+            }
+            */
+
+
+        }
+
+
+        public  void  SemanaDia(string  DiaSemana)
+        {
+            
+
+            switch (DiaSemana)
+            {
+                case "Lunes":
+                    Console.WriteLine("1");
+                    break;
+                    
+                    
+                case "Martes":
+                    Console.WriteLine("2");
+                    break;
+                case "Miercoles":
+                    Console.WriteLine("3");
+                    break;
+                case "Jueves":
+                    Console.WriteLine("4");
+                    break;
+                case "Viernes":
+                    Console.WriteLine("5");    
+                    break;
+                case "Sabado":
+                    Console.WriteLine("6");
+                    break;
+                case "Domingo":
+                    Console.WriteLine("7");
+                    break;
+                default:
+                    Console.WriteLine("No existe ese dia karnal, no inventes cosas");  
+                    break;
+            }
+            
+
+
+        }
+
+        //METODO MIO
+        public void  NumeroPrimo(double numero)
+        {
+
+            if (numero <= 1)
+            {
+                Console.WriteLine("No es primo");
+            }
+            
+            else if (numero % 2 == 0)
+            {
+                Console.WriteLine("No es primo");
+            }
+            else
+            {
+                Console.WriteLine("Es primo");            }
+        }
+
+
+        //METODO DE PROFE
+        public bool EsPrimo(int numero2)
+        {
+            bool esPrimo = true;
+            for(int x=2; x< numero2; x++)
+            {
+                if ( numero2 % x == 0)
+                {
+                    esPrimo= false;
+                }
+            }
+            return esPrimo;
+        }
+
+
+      
+
+        public   double Descuento(double precioOriginal, double porcentajeDescuento)
+        {
+            double resultado;
+            porcentajeDescuento = porcentajeDescuento / 100;
+            resultado= precioOriginal * porcentajeDescuento;
+                
+            return resultado;
+         }
+        public int Factorial (int numero )
+        {
+            if (numero == 1)
+                return numero;
+            numero *= Factorial(numero - 1);
+
+
+
+
+            return numero; }
+
+        public int SumaDescendente(int numero)
+        {
+           
+            if (numero == 0)
+                return numero;
+            
+            return numero+ SumaDescendente(--numero); 
+        
+        }
+
+
+        public bool Palindromo(string numero1)
+        {
+            bool palindromo= true;
+            for (int i=0; i<numero1.Length/2; i++)
+            {
+                if (numero1[i] != numero1[numero1.Length - i - 1])
+                {
+                    palindromo= false;
+                }
+            }
+            return palindromo;
+        }
+
+
+        public int  SumarNumeros(int numeroSuma)
+        {
+
+            int suma = 0;
+            if(numeroSuma < 0)
+            {
+                return suma;
+            }
+            for (int i = 0; i <= numeroSuma; i++)
+            {
+                suma+=i;
+            }
+            return suma;
+        }
+
+
+
+
+
+
+
+        internal string ToString(string? v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace EjerciciosEric20Estructura
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EstructuraControl control1 = new EstructuraControl();
+
+            //EJERCICIO DE QUE SELECCIONAS UN NUMERO Y TE DA EL DIA
+            /*
+            Console.WriteLine("Ingresa el dia que quieres saber");
+            int dia= Convert.ToInt32(Console.ReadLine()); 
+
+            Console.WriteLine(control1.DiaSemana(dia));
+            */
+
+
+            //EJERCICIO QUE TE SACA EL PROMEDIO DE 3 CALIFICACIONES
+            /*
+            Console.WriteLine("Escribe la primera calificacion");
+            int numero1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe la segunda calificacion");
+            int numero2=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Escribe la tercera calificacion");
+            int numero3=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(control1.PromedioNumeros(numero1,numero2,numero3));
+            */
+            //   EJERCICIO DE DIVISIRVE POR 2 O 3
+            /*
+               Console.WriteLine("Escribe el numero");
+               int numero = Convert.ToInt32(Console.ReadLine());
+
+               control1.Divisirve(numero);
+
+
+               /*
+               Console.WriteLine("Escribe la palabra (nota la primera debe de llevar mayuscula y estar bien escrita Ejemeplo: Lunes)");
+               string DiaSemana= Console.ReadLine();
+
+               control1.SemanaDia(DiaSemana);
+
+               //EJERCICIO DE NUMEROS PRIMOS
+
+               Console.WriteLine("Escribe el numero que desees saber si es primo o no");
+               double numero=Convert.ToDouble(Console.ReadLine());
+               control1.NumeroPrimo(numero);
+
+
+               /*PROGRAMA DE DESCUENTOS
+               Console.WriteLine("Escribe el precio original");
+               double precioOriginal = Convert.ToDouble(Console.ReadLine());
+               Console.WriteLine("Escribe el porcentaje de descuento");
+               double porcentajeDescuento = Convert.ToDouble(Console.ReadLine());
+
+               Console.WriteLine(control1.Descuento(precioOriginal, porcentajeDescuento)); 
+               */
+
+            /*
+            Console.WriteLine("Escribe un numero");
+            string numero1=Console.ReadLine();
+            Console.WriteLine(control1.Palindromo(numero1));
+         
+            
+            Console.WriteLine("Escribe el numero que desees saber si es primo o no");
+            int numero2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(control1.EsPrimo(numero2)); 
+
+            */
+            EstructuraControl sumaDescendente = new EstructuraControl();
+            Console.WriteLine("Ingrese un numero");
+
+<<<<<<< HEAD
+
+
+            int numero= Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(sumaDescendente.SumaDescendente(numero));
+=======
+            Console.WriteLine("Escribe un numero");
+            int numeroSuma=Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine(control1.SumarNumeros(numeroSuma));
+
+>>>>>>> 7838efbf07c8768ac9aa318ffb635c22cb28e2b5
+        }
+    }
+}
